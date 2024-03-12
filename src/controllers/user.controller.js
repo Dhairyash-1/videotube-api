4e4681ecca1d99d8abbba526b4fb412df9affe8c
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-03-12 16:45:16 +0530

    fix: mongodId creation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -383,7 +383,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
   const user = await User.aggregate([
     {
       $match: {
-        _id: new mongoose.Schema.Types.ObjectId(req.user?._id), //here we need mongodb id not just string
+        _id: new mongoose.Types.ObjectId(req.user?._id), //here we need mongodb id not just string
       },
     },
     {
