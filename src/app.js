1629d8b25153242e3fe6b496f88c4574cbd5a0c3
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-04-26 16:21:12 +0530

    Add like controller for video,comment and tweet

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 // Routes declaration
 app.use("/api/v1/users", userRouter);
@@ -31,6 +32,7 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
 
 // http://localhost:8000/api/v1/users/register
 
