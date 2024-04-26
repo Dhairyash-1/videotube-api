3f619541d38399e33db0e08ff00d1e1fac3a900e
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-04-26 17:24:46 +0530

    Add dashboard controllers

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // Routes declaration
 app.use("/api/v1/healthCheck", healthcheckRouter);
@@ -35,6 +36,7 @@ app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 // http://localhost:8000/api/v1/users/register
 
---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,103 @@
+import mongoose, { mongo } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getChannelStats = asyncHandler(async (req, res) => {
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: { channel: new mongoose.Types.ObjectId(req.user?._id) },
+    },
+    {
+      $group: {
+        _id: null,
+        totalSubscribersCount: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const videoAggregate = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likeCount: { $size: "$likes" },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalVideos: { $sum: 1 },
+        totalViews: { $sum: "$views" },
+        totalLikes: { $sum: "$likeCount" },
+      },
+    },
+  ]);
+
+  const channelStats = {
+    subscribersCount: subscribers[0].totalSubscribersCount,
+    totalViews: videoAggregate[0]?.totalViews,
+    totalLikes: videoAggregate[0]?.totalLikes,
+    totalVideos: videoAggregate[0]?.totalVideos,
+  };
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channelStats, "Channel Stats fetched successfully")
+    );
+});
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+  const getAllVideos = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: { $size: "$likes" },
+      },
+    },
+    {
+      $project: {
+        likes: 0,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        getAllVideos,
+        "All channel videos fetched successfully"
+      )
+    );
+});
+
+export { getChannelStats, getChannelVideos };
