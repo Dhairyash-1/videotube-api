024e11508fb2ce0e13a454203af712cb12210a22
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-04-26 13:48:19 +0530

    Add Comment controllers and routes

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 // Routes declaration
 app.use("/api/v1/users", userRouter);
@@ -29,6 +30,7 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/comments", commentRouter);
 
 // http://localhost:8000/api/v1/users/register
 
